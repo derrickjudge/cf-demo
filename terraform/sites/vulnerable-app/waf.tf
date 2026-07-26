a886b4db873dd9eae31ec070479bd31d9f637f06
Derrick Judge <derrickjudge@yahoo.com>
2026-07-26 18:52:03 +1000

    Add custom WAF rule blocking the demo's SQLi/XSS payloads

---
+++ terraform/sites/vulnerable-app/waf.tf
@@ -0,0 +1,13 @@
+resource "cloudflare_ruleset" "custom_rules" {
+  zone_id = cloudflare_zone.this.id
+  name    = "Custom rules for cf-demo-vulnerable-app"
+  kind    = "zone"
+  phase   = "http_request_firewall_custom"
+
+  rules = [{
+    ref         = "block_sqli_xss_demo_payloads"
+    description = "Block the specific SQLi/XSS payloads this demo sends"
+    expression  = "(http.request.uri.path eq \"/search\" and http.request.uri.query contains \"'\") or (http.request.uri.path eq \"/greet\" and http.request.uri.query contains \"<script\")"
+    action      = "block"
+  }]
+}
